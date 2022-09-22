5e2422217312733164fda662df2c4dd5b57f7c19
pauguz1 <elsantana5@gmail.com>
2022-09-22 09:16:20 -0500

    se cambio el static file dirs

--- planeacion_docente/settings.py
+++ planeacion_docente/settings.py
@@ -121,8 +121,9 @@ STATIC_URL = 'static/'
 #implementamos los estaticos
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
+
+#'/var/www/static/',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
